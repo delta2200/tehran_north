21c889fbbf287c955cbdcbc7617fe2abfeb1a431
aliordi <aliordi2015@gmail.com>
2021-06-21 15:23:01 +0430

    change url

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,8 @@ app_name = "product"
 urlpatterns = [
     path('' , ProductListView.as_view(), name='product_list'),
     path('type/' , ProductListView.as_view(template_name='product_list_view_type2.html'), name='product_list_type2'),
-    path('<int:id>/detail/' , ProductRawListView.as_view(), name='product_detail'),
+    # path('<int:id>/detail/' , ProductRawListView.as_view(), name='product_detail'),
+    path('<int:id>/detail/' , product_list_view(), name='product_detail'),
     path('<int:id>/delete/' , ProductDeleteView.as_view(), name='product_delete'),
     path('<int:id>/update/' , product_update_view, name='product_update'),
     path('create/' , ProductCreateView.as_view(), name='product_create'),
